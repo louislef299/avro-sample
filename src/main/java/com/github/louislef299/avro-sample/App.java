bc616e356734dba3f2a7b7f6636f1cda18045972
Louis <77028144+louislef299@users.noreply.github.com>
2025-10-03 12:16:17 -0500

    feat: Initial commit

---
+++ src/main/java/com/github/louislef299/avro-sample/App.java
@@ -0,0 +1,10 @@
+package com.github.louislef299.avro-sample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
